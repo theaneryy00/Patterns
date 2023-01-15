a287de46aafbb2d9357f60d5c8f58d81b8a6ad18
theaneryy00 <111473139+theaneryy00@users.noreply.github.com>
2023-01-15 21:04:11 +0800

    Create LeftHalfTriangle.java

---
+++ LeftHalfTriangle.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class App {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a number:");
+        int n =sc.nextInt();
+
+        for (int row = 1; row <= n; row++) {
+            for (int col = 1; col <= row; col++) {
+                
+                    System.out.print("* ");
+                
+            }
+            System.out.println();
+        }
+    }
+}
